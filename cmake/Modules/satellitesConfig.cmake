e34d81799811f16b788bc20e73e12d0b80c94e0c
Daniel Estévez <daniel@destevez.net>
2017-05-12 14:49:15 +0200

    Created OOT module with gr-modtool

---
+++ cmake/Modules/satellitesConfig.cmake
@@ -0,0 +1,30 @@
+INCLUDE(FindPkgConfig)
+PKG_CHECK_MODULES(PC_SATELLITES satellites)
+
+FIND_PATH(
+    SATELLITES_INCLUDE_DIRS
+    NAMES satellites/api.h
+    HINTS $ENV{SATELLITES_DIR}/include
+        ${PC_SATELLITES_INCLUDEDIR}
+    PATHS ${CMAKE_INSTALL_PREFIX}/include
+          /usr/local/include
+          /usr/include
+)
+
+FIND_LIBRARY(
+    SATELLITES_LIBRARIES
+    NAMES gnuradio-satellites
+    HINTS $ENV{SATELLITES_DIR}/lib
+        ${PC_SATELLITES_LIBDIR}
+    PATHS ${CMAKE_INSTALL_PREFIX}/lib
+          ${CMAKE_INSTALL_PREFIX}/lib64
+          /usr/local/lib
+          /usr/local/lib64
+          /usr/lib
+          /usr/lib64
+)
+
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(SATELLITES DEFAULT_MSG SATELLITES_LIBRARIES SATELLITES_INCLUDE_DIRS)
+MARK_AS_ADVANCED(SATELLITES_LIBRARIES SATELLITES_INCLUDE_DIRS)
+
---
+++ swig/satellites_swig.i
@@ -0,0 +1,13 @@
+/* -*- c++ -*- */
+
+#define SATELLITES_API
+
+%include "gnuradio.i"			// the common stuff
+
+//load generated python docstrings
+%include "satellites_swig_doc.i"
+
+%{
+%}
+
+
