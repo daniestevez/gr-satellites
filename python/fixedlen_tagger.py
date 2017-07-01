a91dd3f4e5f9eacca87828b9268f500cd45b8e0a
Daniel Estévez <daniel@destevez.net>
2017-07-01 10:00:50 +0200

    Fixed bug in fixedlen_tagger
    Modifying the self.tags list during the loop was upsetting the loop.
    Also some minor improvements that could potentially cause bugs.

--- python/fixedlen_tagger.py
+++ python/fixedlen_tagger.py
@@ -37,10 +37,12 @@ class fixedlen_tagger(gr.basic_block):
         self.packetlen_tag = pmt.string_to_symbol(packetlen_tag)
         self.packet_len = packet_len
         self.stream = collections.deque(maxlen=packet_len - 1)
-        self.maxtag = 0
+        self.maxtag = -1
         self.data = []
         self.tags = []
+        self.tags_to_write = []
         self.written = 0
+        self.really_written = 0
 
     def general_work(self, input_items, output_items):
         inp = input_items[0]
@@ -48,24 +50,33 @@ class fixedlen_tagger(gr.basic_block):
 
         window = list(self.stream) + inp.tolist()
         
-        alltags = self.get_tags_in_range(0, self.maxtag, self.nitems_read(0) + len(inp), self.syncword_tag)
+        alltags = self.get_tags_in_range(0, self.maxtag + 1, self.nitems_read(0) + len(inp), self.syncword_tag)
         for tag in alltags:
             if tag.offset not in self.tags:
                 self.maxtag = max(self.maxtag, tag.offset)
                 self.tags.append(tag.offset)
-        for tag in self.tags:
+        tags = self.tags[:] # modifying self.tags during the loop could disturb the loop control, so we copy it
+        for tag in tags:
             if (tag >= self.nitems_read(0) - len(self.stream)) and (tag < self.nitems_read(0) + len(inp) - self.packet_len + 1):
                 self.tags.remove(tag)
                 start = tag - self.nitems_read(0) + len(self.stream)
                 packet = window[start : start + self.packet_len]
                 self.data += packet
-                self.add_item_tag(0, self.written, self.packetlen_tag, pmt.from_long(self.packet_len))
+                self.tags_to_write.append(self.written)
                 self.written += self.packet_len
 
         self.stream.extend(inp.tolist())
 
-        len_write = min(len(self.data), len(output_items[0]))
-        output_items[0][:len_write] = self.data[:len_write]
+        len_write = min(len(self.data), len(out))
+        out[:len_write] = self.data[:len_write]
         self.data = self.data[len_write:]
+        self.really_written += len_write
+
+        tags = self.tags_to_write[:] # again, this is to avoid disturbing the loop
+        for tag in tags:
+            if tag < self.really_written:
+                self.tags_to_write.remove(tag)
+                self.add_item_tag(0, tag, self.packetlen_tag, pmt.from_long(self.packet_len))
+                
         self.consume(0, len(inp))
         return len_write
