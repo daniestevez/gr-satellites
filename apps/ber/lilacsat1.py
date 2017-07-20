467cc8c1368044e9d671d904be999f866725665b
Daniel Estévez <daniel@destevez.net>
2017-07-20 22:56:31 +0200

    Added BER test for LilacSat-1

---
+++ apps/ber/lilacsat1.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python2
+#
+# Copyright 2012,2013 Free Software Foundation, Inc.
+#
+# This file is part of GNU Radio
+#
+# GNU Radio is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+#
+# GNU Radio is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with GNU Radio; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street,
+# Boston, MA 02110-1301, USA.
+#
+
+import math
+import numpy
+from gnuradio import gr, digital
+from gnuradio import analog
+from gnuradio import blocks
+from gnuradio import filter
+from gnuradio.filter import firdes
+from gnuradio import channels
+from gnuradio import fec
+import sys
+
+try:
+    from scipy.special import erfc
+except ImportError:
+    print "Error: could not import scipy (http://www.scipy.org/)"
+    sys.exit(1)
+
+try:
+    import pylab
+except ImportError:
+    print "Error: could not import pylab (http://matplotlib.sourceforge.net/)"
+    sys.exit(1)
+
+from lilacsat1_ber_bpsk import *
+from lilacsat1_ber_viterbi import *
+    
+# Best to choose powers of 10
+N_BITS = 1e8
+SKIP = 1000
+RAND_SEED = 42
+
+def berawgn(EbN0):
+    """ Calculates theoretical bit error rate in AWGN (for BPSK and given Eb/N0) """
+    return 0.5 * erfc(math.sqrt(10**(float(EbN0)/10)))
+
+class BitErrors(gr.hier_block2):
+    """test"""
+    def __init__(self):
+        gr.hier_block2.__init__(self, "BitErrors",
+                gr.io_signature(1, 1, gr.sizeof_char),
+                gr.io_signature(1, 1, gr.sizeof_int))
+
+        intdump_decim = min(int(N_BITS/10), 100000)
+        self.connect(self,
+                     blocks.skiphead(gr.sizeof_char, SKIP),
+                     blocks.not_bb(),
+                     blocks.and_const_bb(1),
+                     blocks.uchar_to_float(),
+                     blocks.integrate_ff(intdump_decim),
+                     blocks.multiply_const_ff(1.0/intdump_decim),
+                     self)
+
+class BERAWGNSimu(gr.top_block):
+    " This contains the simulation flow graph "
+    def __init__(self, EbN0, viterbi=False):
+        gr.top_block.__init__(self)
+
+        self.sps = 5
+        alpha = 0.35
+
+        const = digital.constellation_bpsk().base()
+        modulator = digital.generic_mod(
+          constellation=const,
+          differential=False,
+          samples_per_symbol=self.sps,
+          pre_diff_code=True,
+          excess_bw=alpha,
+          verbose=False,
+          log=False,
+          )
+        channel = channels.channel_model(
+        	noise_voltage=self.EbN0_to_noise_voltage(EbN0, viterbi),
+        	frequency_offset=0,
+        	epsilon=1.0,
+        	taps=(0, 0, (1+1j)/numpy.sqrt(2), ),
+        	noise_seed=RAND_SEED,
+        	block_tags=False
+        )
+        self.sink  = blocks.vector_sink_f()
+        biterrors = BitErrors()
+
+        dut = lilacsat1_ber_viterbi() if viterbi else lilacsat1_ber_bpsk()
+
+        pack = blocks.pack_k_bits_bb(8)
+        descrambler = digital.descrambler_bb(0x21, 0x00, 16)
+        self.connect(blocks.vector_source_b([1], repeat=True),
+                     blocks.head(gr.sizeof_char, int(N_BITS)),
+                     digital.scrambler_bb(0x21, 0x00, 16),
+                     digital.diff_encoder_bb(2),
+                     pack)
+        self.connect(modulator, channel,
+                     blocks.multiply_const_cc(0.5), # signal amplitude 1 seems very important
+                     dut,
+                     digital.diff_decoder_bb(2),
+                     descrambler)
+        self.connect(biterrors, self.sink)
+        
+        if viterbi:
+            deinterleave_viterbi = blocks.deinterleave(gr.sizeof_char)
+            interleave_viterbi = blocks.interleave(gr.sizeof_char)
+            self.connect(pack,
+                         fec.encode_ccsds_27_bb(),
+                         deinterleave_viterbi)
+            self.connect((deinterleave_viterbi, 0),
+                         (interleave_viterbi, 1))
+            self.connect((deinterleave_viterbi, 1),
+                         blocks.not_bb(),
+                         blocks.and_const_bb(1),
+                         (interleave_viterbi, 0))
+            self.connect(interleave_viterbi,
+                         blocks.pack_k_bits_bb(8),
+                         modulator)
+            descrambler2 = digital.descrambler_bb(0x21, 0x00, 16)
+            self.connect((dut, 1),
+                        digital.diff_decoder_bb(2),
+                        descrambler2)
+            or2 = blocks.or_bb()
+            self.connect(descrambler, or2)
+            self.connect(descrambler2, (or2, 1))
+            self.connect(or2, biterrors)
+            #self.sinkviterbi1 = blocks.vector_sink_b()
+            #self.sinkviterbi2 = blocks.vector_sink_b()
+            #self.connect(descrambler, self.sinkviterbi1)
+            #self.connect(descrambler2, self.sinkviterbi2)
+
+        else:
+            self.connect(pack, modulator)
+            self.connect(descrambler, biterrors)            
+
+    def EbN0_to_noise_voltage(self, EbN0, viterbi):
+        """ Converts Eb/N0 to a complex noise voltage (assuming unit symbol power) """
+        spb = float(self.sps)*2.0 if viterbi else float(self.sps) # samples per bit
+        return 1.0 / math.sqrt(1/spb * 10**(float(EbN0)/10))
+
+def simulate_ber(EbN0, viterbi = False):
+    """ All the work's done here: create flow graph, run, read out BER """
+    print "Eb/N0 = %d dB" % EbN0
+    fg = BERAWGNSimu(EbN0, viterbi)
+    fg.run()
+    data = fg.sink.data()
+    factor = 3.0 if viterbi else 6.0 # each bit error produces 6 errors, taking into account differential coding and scrambler
+    return numpy.sum(data)/(factor*len(data))
+
+if __name__ == "__main__":
+    EbN0_min = 0
+    EbN0_max = 12
+    EbN0_range = range(EbN0_min, EbN0_max+1)
+    ber_theory = [berawgn(x)      for x in EbN0_range]
+    print "Simulating uncoded BPSK..."
+    ber_simu_bpsk   = [simulate_ber(x, False) for x in EbN0_range]
+    print "Simulating Viterbi..."
+    ber_simu_viterbi   = [simulate_ber(x, True) for x in EbN0_range]
+
+    f = pylab.figure()
+    s = f.add_subplot(1,1,1)
+    s.semilogy(EbN0_range, ber_theory, 'g-.', label="Theoretical uncoded BPSK")
+    s.semilogy(EbN0_range, ber_simu_bpsk, 'b-o', label="Simulated uncoded BPSK")
+    s.semilogy(EbN0_range, ber_simu_viterbi, 'r-o', label="Simulated Viterbi")
+    s.set_title('BER Simulation')
+    s.set_xlabel('Eb/N0 (dB)')
+    s.set_ylabel('BER')
+    s.legend()
+    s.grid()
+    pylab.show()
---
+++ apps/ber/lilacsat1_ber_viterbi.py
@@ -0,0 +1,197 @@
+# -*- coding: utf-8 -*-
+##################################################
+# GNU Radio Python Flow Graph
+# Title: LilacSat-1 decoder
+# Author: Daniel Estevez
+# Description: LilacSat-1 decoder
+# Generated: Tue Jul 18 23:45:01 2017
+##################################################
+
+import os
+import sys
+sys.path.append(os.environ.get('GRC_HIER_PATH', os.path.expanduser('~/.grc_gnuradio')))
+
+from ccsds_viterbi import ccsds_viterbi  # grc-generated hier_block
+from gnuradio import analog
+from gnuradio import blocks
+from gnuradio import digital
+from gnuradio import filter
+from gnuradio import gr
+from gnuradio.filter import firdes
+
+
+class lilacsat1_ber_viterbi(gr.hier_block2):
+
+    def __init__(self, bfo=12000, callsign='', ip='::', latitude=0, longitude=0, port=7355, recstart=''):
+        gr.hier_block2.__init__(
+            self, "LilacSat-1 decoder",
+            gr.io_signature(1, 1, gr.sizeof_gr_complex*1),
+            gr.io_signaturev(2, 2, [gr.sizeof_char*1, gr.sizeof_char*1]),
+        )
+
+        ##################################################
+        # Parameters
+        ##################################################
+        self.bfo = bfo
+        self.callsign = callsign
+        self.ip = ip
+        self.latitude = latitude
+        self.longitude = longitude
+        self.port = port
+        self.recstart = recstart
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.sps = sps = 5
+        self.samp_per_sym = samp_per_sym = 5
+        self.nfilts = nfilts = 16
+        self.alpha = alpha = 0.35
+
+        self.variable_constellation_0_0 = variable_constellation_0_0 = digital.constellation_calcdist(([-1, 1]), ([0, 1]), 2, 1).base()
+
+        self.threshold = threshold = 3
+        self.samp_rate = samp_rate = 48000
+        self.rrc_taps_0 = rrc_taps_0 = firdes.root_raised_cosine(nfilts, nfilts, 1.0/float(samp_per_sym), 0.35, 11*samp_per_sym*nfilts)
+        self.rrc_taps = rrc_taps = firdes.root_raised_cosine(nfilts, nfilts, 1.0/float(sps), alpha, 11*sps*nfilts)
+        self.nfilts_0 = nfilts_0 = 16
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.low_pass_filter_0_0 = filter.fir_filter_ccf(1, firdes.low_pass(
+        	1, samp_rate, 10000, 1000, firdes.WIN_HAMMING, 6.76))
+        self.low_pass_filter_0 = filter.fir_filter_ccf(1, firdes.low_pass(
+        	1, samp_rate, 6000, 500, firdes.WIN_HAMMING, 6.76))
+        self.digital_pfb_clock_sync_xxx_0 = digital.pfb_clock_sync_ccf(sps, 0.05, (rrc_taps), nfilts, nfilts/2, 0.01, 1)
+        self.digital_fll_band_edge_cc_0 = digital.fll_band_edge_cc(sps, 0.350, 100, 0.001)
+        self.digital_costas_loop_cc_0_0 = digital.costas_loop_cc(0.1, 2, False)
+        self.ccsds_viterbi_0_0 = ccsds_viterbi()
+        self.ccsds_viterbi_0 = ccsds_viterbi()
+        self.blocks_delay_0 = blocks.delay(gr.sizeof_float*1, 1)
+        self.blocks_complex_to_real_0 = blocks.complex_to_real(1)
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.blocks_complex_to_real_0, 0), (self.blocks_delay_0, 0))
+        self.connect((self.blocks_complex_to_real_0, 0), (self.ccsds_viterbi_0, 0))
+        self.connect((self.blocks_delay_0, 0), (self.ccsds_viterbi_0_0, 0))
+        self.connect((self.ccsds_viterbi_0, 0), (self, 0))
+        self.connect((self.ccsds_viterbi_0_0, 0), (self, 1))
+        self.connect((self.digital_costas_loop_cc_0_0, 0), (self.blocks_complex_to_real_0, 0))
+        self.connect((self.digital_fll_band_edge_cc_0, 0), (self.low_pass_filter_0, 0))
+        self.connect((self.digital_pfb_clock_sync_xxx_0, 0), (self.digital_costas_loop_cc_0_0, 0))
+        self.connect((self, 0), (self.low_pass_filter_0_0, 0))
+        self.connect((self.low_pass_filter_0, 0), (self.digital_pfb_clock_sync_xxx_0, 0))
+        self.connect((self.low_pass_filter_0_0, 0), (self.digital_fll_band_edge_cc_0, 0))
+
+    def get_bfo(self):
+        return self.bfo
+
+    def set_bfo(self, bfo):
+        self.bfo = bfo
+
+    def get_callsign(self):
+        return self.callsign
+
+    def set_callsign(self, callsign):
+        self.callsign = callsign
+
+    def get_ip(self):
+        return self.ip
+
+    def set_ip(self, ip):
+        self.ip = ip
+
+    def get_latitude(self):
+        return self.latitude
+
+    def set_latitude(self, latitude):
+        self.latitude = latitude
+
+    def get_longitude(self):
+        return self.longitude
+
+    def set_longitude(self, longitude):
+        self.longitude = longitude
+
+    def get_port(self):
+        return self.port
+
+    def set_port(self, port):
+        self.port = port
+
+    def get_recstart(self):
+        return self.recstart
+
+    def set_recstart(self, recstart):
+        self.recstart = recstart
+
+    def get_sps(self):
+        return self.sps
+
+    def set_sps(self, sps):
+        self.sps = sps
+        self.set_rrc_taps(firdes.root_raised_cosine(self.nfilts, self.nfilts, 1.0/float(self.sps), self.alpha, 11*self.sps*self.nfilts))
+
+    def get_samp_per_sym(self):
+        return self.samp_per_sym
+
+    def set_samp_per_sym(self, samp_per_sym):
+        self.samp_per_sym = samp_per_sym
+        self.set_rrc_taps_0(firdes.root_raised_cosine(self.nfilts, self.nfilts, 1.0/float(self.samp_per_sym), 0.35, 11*self.samp_per_sym*self.nfilts))
+
+    def get_nfilts(self):
+        return self.nfilts
+
+    def set_nfilts(self, nfilts):
+        self.nfilts = nfilts
+        self.set_rrc_taps(firdes.root_raised_cosine(self.nfilts, self.nfilts, 1.0/float(self.sps), self.alpha, 11*self.sps*self.nfilts))
+        self.set_rrc_taps_0(firdes.root_raised_cosine(self.nfilts, self.nfilts, 1.0/float(self.samp_per_sym), 0.35, 11*self.samp_per_sym*self.nfilts))
+
+    def get_alpha(self):
+        return self.alpha
+
+    def set_alpha(self, alpha):
+        self.alpha = alpha
+        self.set_rrc_taps(firdes.root_raised_cosine(self.nfilts, self.nfilts, 1.0/float(self.sps), self.alpha, 11*self.sps*self.nfilts))
+
+    def get_variable_constellation_0_0(self):
+        return self.variable_constellation_0_0
+
+    def set_variable_constellation_0_0(self, variable_constellation_0_0):
+        self.variable_constellation_0_0 = variable_constellation_0_0
+
+    def get_threshold(self):
+        return self.threshold
+
+    def set_threshold(self, threshold):
+        self.threshold = threshold
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.low_pass_filter_0_0.set_taps(firdes.low_pass(1, self.samp_rate, 10000, 1000, firdes.WIN_HAMMING, 6.76))
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, 6000, 500, firdes.WIN_HAMMING, 6.76))
+
+    def get_rrc_taps_0(self):
+        return self.rrc_taps_0
+
+    def set_rrc_taps_0(self, rrc_taps_0):
+        self.rrc_taps_0 = rrc_taps_0
+
+    def get_rrc_taps(self):
+        return self.rrc_taps
+
+    def set_rrc_taps(self, rrc_taps):
+        self.rrc_taps = rrc_taps
+        self.digital_pfb_clock_sync_xxx_0.update_taps((self.rrc_taps))
+
+    def get_nfilts_0(self):
+        return self.nfilts_0
+
+    def set_nfilts_0(self, nfilts_0):
+        self.nfilts_0 = nfilts_0
