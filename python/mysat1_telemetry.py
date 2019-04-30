cb5f5975e41a63b65a55d87f5c5a7b0989d8ea58
Hamze Issa <hamze.j.i@gmail.com>
2019-04-30 12:09:14 +0400

    added missing telemetry values and fixed endianess issue

--- python/mysat1_telemetry.py
+++ python/mysat1_telemetry.py
@@ -22,70 +22,72 @@
 from construct import *
 from adapters import *
 
-Timestamp = UNIXTimestampAdapter(Int32ub)
+Timestamp = UNIXTimestampAdapter(Int32ul)
 
 Callsign = Struct(
-    'callsign' /  Int8ub[5])
+    'callsign' /  Bytes(5))
 
 OBC = Struct(
-    'obc_mode' / Int8ub,
-    'obc_reset_counter' / Int32ub,
-    'obc_uptime' / Int32ub)
+    'obc_mode' / Int8ul,
+    'obc_reset_counter' / Int32ul,
+    'obc_uptime' / Int32ul)
 
 Gyro = Struct(
-    'gyro_norm' / Int8ub)
+    'gyro_norm' / Int8ul)
+
+EPS = Struct(
+    'eps_counter_boot' / Int32ul,
+    'eps_last_boot_cause' / Int8ul,
+    'eps_battery_mode' / Int8ul)
 
 Timestamp_Struct = Struct(
     'timestamp' / Timestamp)
 
 OBC_Temp = Struct(
-    'obc_temp' / AffineAdapter(1, 128, Int8ub),
-    'obc_daughter_board_temp' / AffineAdapter(1, 128, Int8ub))
+    'obc_temp' / AffineAdapter(1, 128, Int8ul),
+    'obc_daughter_board_temp' / AffineAdapter(1, 128, Int8ul))
 
 EPS_Temp = Struct(
-    'eps_battery_temp' / AffineAdapter(1, 128, Int8ub),
-    'eps_board_temp' / AffineAdapter(1, 128, Int8ub))
+    'eps_battery_temp' / AffineAdapter(1, 128, Int8ul),
+    'eps_board_temp' / AffineAdapter(1, 128, Int8ul))
 
 Ants = Struct(
-    'ants_temp' / AffineAdapter(1, 128, Int8ub))
+    'ants_temp' / AffineAdapter(1, 128, Int8ul))
 
 TRXVU_Temp = Struct(
-    'trxvu_temp' / AffineAdapter(1, 128, Int8ub))
+    'trxvu_temp' / AffineAdapter(1, 128, Int8ul))
 
 ADCS = Struct(
-    'adcs_temp' / AffineAdapter(1, 128, Int8ub))
-
-Solar_Panels = Struct(
-    'solar_panels_temp' / AffineAdapter(1, 128, Int8ub)[6])
+    'adcs_temp' / AffineAdapter(1, 128, Int8ul))
 
 OBC_Voltages = Struct(
-'obc_3v3_voltage' / LinearAdapter(10.0, Int8ub),
-'obc_5v0_voltage' / LinearAdapter(10.0, Int8ub))
+'obc_3v3_voltage' / LinearAdapter(10.0, Int8ul),
+'obc_5v0_voltage' / LinearAdapter(10.0, Int8ul))
 
 TRXVU_Voltage = Struct(
-'trxvu_voltage' / LinearAdapter(10.0, Int8ub))
+'trxvu_voltage' / LinearAdapter(10.0, Int8ul))
 
 EPS_Batt_Voltage = Struct(
-'eps_batt_voltage' / LinearAdapter(10.0, Int8ub))
+'eps_batt_voltage' / LinearAdapter(10.0, Int8ul))
 
 OBC_Current = Struct(
-'obc_5.0_current' / Int16ub)
+'obc_5.0_current' / LinearAdapter(1000.0, Int16ul))
 
 EPS_Currents = Struct(
-'eps_total_pv_current' / Int16ub,
-'eps_total_system_current' / Int16ub)
+'eps_total_pv_current' / LinearAdapter(1000.0, Int16ul),
+'eps_total_system_current' / LinearAdapter(1000.0, Int16ul))
 
 Beacon0 = Struct(
     'callsign' / Callsign,
     'obc' / OBC,
     'gyro' / Gyro,
+    'eps' / EPS,
     'timestamp' / Timestamp_Struct,
     'obc_temp' / OBC_Temp,
     'eps_temp' / EPS_Temp,
     'ants' / Ants,
     'trxvu_temp' / TRXVU_Temp,
     'adcs' / ADCS,
-    'solar_panels' / Solar_Panels,
     'obc_voltages' / OBC_Voltages,
     'trxvu_voltage' / TRXVU_Voltage,
     'eps_batt_voltage' / EPS_Batt_Voltage,
