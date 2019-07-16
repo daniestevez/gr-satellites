daniestevez/gr-satellites

commit c372d3ab4041aeec8c9650a1d2678cc1c1672e4c

Ignore unknown frame types in funcube_telemetry_parser
Before, an unknown frame type produced an unhandled exception

python/funcube_telemetry_parser.py
@@ -25,6 +25,8 @@ import pmt
 import funcube_telemetry
 import struct
 
+import construct
+
 WHOLEORBIT_SIZE = 23
 PAYLOAD_SIZE = 200
 WHOLEORBIT_MAX = 12
@@ -57,6 +59,9 @@ class funcube_telemetry_parser(gr.basic_block):
         data = funcube_telemetry.beacon_parse(packet)
         if data:
             print 'Frame type {}'.format(data.header.frametype)
+            if not data.header.frametype is construct.EnumIntegerString:
+                print 'Unknown frame type. Not processing frame.'
+                return
             print '-'*40
             print 'Realtime telemetry:'
             print '-'*40

commit 2eb31d43c400a48d7b48e1f31df3d58201801fbb

Fix bug in FUNcube telemetry parsing
Fixes a bug introduced in c372d3ab4041aeec8c9650a1d2678cc1c1672e4c
This bug made frame types to be recognized incorrectly, so frames were
not parsed.

python/funcube_telemetry.py
@@ -413,7 +413,7 @@ Frame = Struct(
             0x08 : RealTimeNayif1,
             }, default = Bytes(54)),
         }),
-    'payload' / If(type(this.header.frametype) is EnumIntegerString,
+    'payload' / If(not isinstance(this.header.frametype, int),
                    Switch(lambda c: c.header.frametype[:2], {
                     'WO' : Bytes(200),
                     'HR' : HRPayload,

python/funcube_telemetry_parser.py
@@ -59,7 +59,7 @@ class funcube_telemetry_parser(gr.basic_block):
         data = funcube_telemetry.beacon_parse(packet)
         if data:
             print 'Frame type {}'.format(data.header.frametype)
-            if not data.header.frametype is construct.EnumIntegerString:
+            if isinstance(data.header.frametype, int):
                 print 'Unknown frame type. Not processing frame.'
                 return
             print '-'*40