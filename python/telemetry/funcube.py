daniestevez/gr-satellites

commit 220259b7eb4ade2852a135621efc1ed52be5680d

Fix bug in FUNcube telemetry parsing
For some weird reason, using this instead of lambda to check
if the frame type is correct didn't work.

Now we are also asking for __getitem__ (which is what we use)
instead of whether the frame is an instance of int. This seems more
robust.

This is taken from 739554562a06978eab3c8f86738cb27928ba0b32

python/telemetry/funcube.py
@@ -411,7 +411,7 @@ Frame = Struct(
             0x08 : RealTimeNayif1,
             }, default = Bytes(54)),
         }),
-    'payload' / If(not isinstance(this.header.frametype, int),
+    'payload' / If(lambda: hasattr(this.header.frametype, '__getitem__'),
                    Switch(lambda c: c.header.frametype[:2], {
                     'WO' : Bytes(200),
                     'HR' : HRPayload,

python/funcube_telemetry_parser.py
@@ -63,7 +63,7 @@ class funcube_telemetry_parser(gr.basic_block):
 #            return
         if data:
             print('Frame type {}'.format(data.header.frametype))
-            if isinstance(data.header.frametype, int):
+            if not hasattr(data.header.frametype, '__getitem__'):
                 print('Unknown frame type. Not processing frame.')
                 return
             print('-'*40)

commit e4ec1a1e69f9034160cdc3f39426bce6776814b8

Correct bug in FUNcube telemetry parser
This corrects a wrong lambda expression introduced in
220259b7eb4ade2852a135621efc1ed52be5680d

python/telemetry/funcube.py
@@ -411,7 +411,7 @@ Frame = Struct(
             0x08 : RealTimeNayif1,
             }, default = Bytes(54)),
         }),
-    'payload' / If(lambda: hasattr(this.header.frametype, '__getitem__'),
+    'payload' / If(lambda c: hasattr(c.header.frametype, '__getitem__'),
                    Switch(lambda c: c.header.frametype[:2], {
                     'WO' : Bytes(200),
                     'HR' : HRPayload,