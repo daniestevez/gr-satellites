13fe84c59e3ed3deb1c7272555d9320f9722c3ec
Daniel Estévez <daniel@destevez.net>
2020-09-05 16:41:31 +0200

    Add ZMQ PUB socket output
    This sends decoded PDUs using a ZMQ PUB socket
    
    See #145

--- python/core/gr_satellites_flowgraph.py
+++ python/core/gr_satellites_flowgraph.py
@@ -8,7 +8,7 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
 
-from gnuradio import gr
+from gnuradio import gr, zeromq
 from ..components import demodulators
 from ..components import deframers
 from ..components import datasinks
@@ -128,6 +128,8 @@ class gr_satellites_flowgraph(gr.hier_block2):
                 self._additional_datasinks.append(datasinks.kiss_file_sink(options.kiss_out, bool(options.kiss_append)))
             if options is not None and options.kiss_server:
                 self._additional_datasinks.append(datasinks.kiss_server_sink(options.kiss_server))
+            if options is not None and options.zmq_pub:
+                self._additional_datasinks.append(zeromq.pub_msg_sink(options.zmq_pub))
             if config.getboolean('Groundstation', 'submit_tlm'):
                 self._additional_datasinks.extend(self.get_telemetry_submitters(satyaml, config))
             self._transports = dict()
