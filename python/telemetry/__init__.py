a8fd4d5386a8a2cab3d2e854b0b2649e6731afa6
gromes <gromes.jan@gmail.com>
2020-10-10 17:55:17 +0200

    Added FOSSASAT-1B/2 telemetry and YAML

--- python/telemetry/__init__.py
+++ python/telemetry/__init__.py
@@ -30,6 +30,8 @@ from .csp import csp_swapped
 from .dstar_one import dstar_one
 from .eseo import eseo
 from .floripasat import floripasat
+from .fossasat_1b import fossasat_1b
+from .fossasat_2 import fossasat_2
 from .funcube import funcube
 from .gomx_1 import gomx_1
 from .gomx_3 import gomx_3
