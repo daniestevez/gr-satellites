daniestevez/gr-satellites

commit 65b51a8487e773d2f7edc83adfd2ca84413bd40b

Do not invert byte order for SALSAT CRC calculation
This makes SALSAT CRC calculation work and fixes #168

The problem with the previous SALSAT CRC calculation is that
we had missed to identify one additional change between the
SNET and SALSAT CRC calculations: the SNET calculation pushes
the bytes to the CRC in the reverse order, while SALSAT does
not do that.

python/snet_deframer.py
@@ -51,7 +51,11 @@ class snet_deframer(gr.basic_block):
         ltu = np.fliplr(ltu[:,-5:]).ravel()
         hdr = LTUFrameHeader.parse(np.packbits(ltu))
 
-        ltu_crc = np.flipud(np.concatenate((ltu[:-5], np.array([1,0,1,1,0,1,1]))).reshape((9,8)))
+        ltu_crc = np.concatenate((ltu[:-5], np.array([1,0,1,1,0,1,1]))).reshape((9,8))
+        if self.buggy_crc:
+            # reverse byte ordering for CRC5 calculation
+            ltu_crc = np.flipud(lut_crc)
+
         if self.buggy_crc:
             # force CRC5 bugs
             ltu_crc[4,:] = ltu_crc[3,:]
@@ -128,7 +132,8 @@ class snet_deframer(gr.basic_block):
 
         # crc13
         crc = 0x1FFF
-        for bit in np.flipud(pdu_bytes).ravel():
+        pdu_crc = np.flipud(pdu_bytes) if self.buggy_crc else pdu_bytes
+        for bit in pdu_crc.ravel():
             c = crc & 0x1000 # check most significant bit in the CRC buffer and safe in a variable.
             c >>= 12 # shift variable to make the compare op. possible (see beneath).
             crc <<= 1 # shift CRC to the left and write 0 into the least significant bit.

commit 753e1553c81c6dc97e62fde9b5b441657404e6b3

Fix bug in S-NET deframer
This bug was introduced by a typo in
65b51a8487e773d2f7edc83adfd2ca84413bd40b

This fixes #184

python/snet_deframer.py
@@ -54,7 +54,7 @@ class snet_deframer(gr.basic_block):
         ltu_crc = np.concatenate((ltu[:-5], np.array([1,0,1,1,0,1,1]))).reshape((9,8))
         if self.buggy_crc:
             # reverse byte ordering for CRC5 calculation
-            ltu_crc = np.flipud(lut_crc)
+            ltu_crc = np.flipud(ltu_crc)
 
         if self.buggy_crc:
             # force CRC5 bugs