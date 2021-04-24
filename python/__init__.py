ad518b1570d6bdb863348d0c3666dceff17fd803
Daniel Estévez <daniel@destevez.net>
2021-04-24 11:20:54 +0200

    Up to v4.1.0

--- python/__init__.py
+++ python/__init__.py
@@ -19,7 +19,7 @@ stack, the AO-40 protocol used in the FUNcube satellites, and several
 ad-hoc protocols used in other satellites.
 """
 
-__version__ = 'v4.0.0'
+__version__ = 'v4.1.0'
 __author__ = 'Daniel Estevez'
 __copyright__ = 'Copyright 2016-2020 Daniel Estevez'
 __email__ = 'daniel@destevez.net'
