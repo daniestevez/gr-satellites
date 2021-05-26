8bc445605f70b1f1affdfbd98302e0eb8116114c
Daniel Estévez <daniel@destevez.net>
2021-05-26 22:57:35 +0200

    Add MIR-SAT1 telemetry conversion formulas
    (cherry picked from commit ef7e4477d59bf5c080f2fa1f8ed737be7bb4737f)

--- python/telemetry/mirsat1.py
+++ python/telemetry/mirsat1.py
@@ -10,6 +10,7 @@
 
 from construct import *
 
+from ..adapters import LinearAdapter, AffineAdapter
 from .ax25 import Header as AX25Header
 
 
@@ -31,29 +32,73 @@ BeaconA = BitStruct(
     'i2c_hw_state_err_count' / BitsInteger(32),
     'i2c_isr_err_count' / BitsInteger(32),
     'battery_current_dir' / Enum(BitsInteger(1), discharging=0, charging=1),
-    'battery_current' / BitsInteger(10),
+    'battery_current' / LinearAdapter(1/1.327547, BitsInteger(10)),
     'battery_current_msb' / BitsInteger(1)
     )
 
+
+class SignMagnitudeAdapter16(Adapter):
+    def _encode(self, obj, context, path=None):
+        mag = abs(obj) & 0x7fff
+        sig = 1 if obj < 0 else 0
+        return (sig << 15) | mag
+
+    def _decode(self, obj, context, path=None):
+        mag = obj & 0x7fff
+        return -mag if obj & 0x8000 else mag
+
+
+SignMagnitude16 = SignMagnitudeAdapter16(Int16ub)
+
+
+class SignMagnitudeAdapter8(Adapter):
+    def _encode(self, obj, context, path=None):
+        mag = abs(obj) & 0x7f
+        sig = 1 if obj < 0 else 0
+        return (sig << 7) | mag
+
+    def _decode(self, obj, context, path=None):
+        mag = obj & 0x7f
+        return -mag if obj & 0x80 else mag
+
+
+SignMagnitude8 = SignMagnitudeAdapter8(Int8ub)
+
 BeaconPartB = BitStruct(
     'battery_current_lsbs' / BitsInteger(9),
-    'battery_current' / BitsInteger(10),
-    'battery_voltage' / BitsInteger(10)[3],
-    'battery_temp' / BitsInteger(10),
-    'solar_current' / BitsInteger(10),
-    'solar_voltage' / BitsInteger(10)[9],  # last 5 unused
-    'eps_bus_voltage' / BitsInteger(10)[4],
-    'eps_bus_current' / BitsInteger(10)[4],
-    'adcs_raw_gyro' / Bytewise(Int16ub[3]),
+    'battery_current' / LinearAdapter(1/14.662757, BitsInteger(10)),
+    'battery_voltage_0' / LinearAdapter(1/0.004311, BitsInteger(10)),
+    'battery_voltage_1' / LinearAdapter(1/0.005865, BitsInteger(10)),
+    'battery_voltage_2' / LinearAdapter(1/0.008993, BitsInteger(10)),
+    'battery_temp' / AffineAdapter(1/0.372434, 273.15/0.372434,
+                                   BitsInteger(10)),
+    'solar_current' / LinearAdapter(1/0.977517107, BitsInteger(10)),
+    # Last 5 solar_voltage's unused
+    'solar_voltage' / LinearAdapter(1/0.009971, BitsInteger(10))[9],
+    'eps_bus_voltage_0' / LinearAdapter(1/0.008978, BitsInteger(10)),
+    'eps_bus_voltage_1' / LinearAdapter(1/0.00431085, BitsInteger(10)),
+    'eps_bus_voltage_2' / LinearAdapter(1/0.005865103, BitsInteger(10)),
+    'eps_bus_voltage_3' / LinearAdapter(1/0.013489736, BitsInteger(10)),
+    'eps_bus_current_0' / LinearAdapter(1/2.07, BitsInteger(10)),
+    'eps_bus_current_1' / LinearAdapter(1/5.236698785, BitsInteger(10)),
+    'eps_bus_current_2' / LinearAdapter(1/5.236698785, BitsInteger(10)),
+    'eps_bus_current_3' / LinearAdapter(1/2.07, BitsInteger(10)),
+    'adcs_raw_gyro' / Bytewise(LinearAdapter(1, SignMagnitude16)[3]),
     'adcs_mtq_dir_duty' / BitsInteger(8)[6],
     'adcs_status' / BitsInteger(16),
-    'adcs_bus_voltage' / BitsInteger(16)[3],
-    'adcs_bus_current' / BitsInteger(16)[3],
-    'adcs_board_temp' / BitsInteger(16),
+    'adcs_bus_voltage_0' / LinearAdapter(1/0.000152587, BitsInteger(16)),
+    'adcs_bus_voltage_1' / LinearAdapter(1/0.0000944, BitsInteger(16)),
+    'adcs_bus_voltage_2' / LinearAdapter(1/0.000152587, BitsInteger(16)),
+    'adcs_bus_current_0' / LinearAdapter(1/0.00000600472, BitsInteger(16)),
+    'adcs_bus_current_1' / LinearAdapter(1/0.0000381, BitsInteger(16)),
+    'adcs_bus_current_2' / LinearAdapter(1/0.0000244230769230769,
+                                         BitsInteger(16)),
+    'adcs_board_temp' / LinearAdapter(1/0.00762, BitsInteger(16)),
     'adcs_adc_ref' / BitsInteger(16),
     'adcs_sensor_current' / BitsInteger(16),
     'adcs_mtq_current' / BitsInteger(16),
-    'adcs_array_temp' / BitsInteger(16)[6],
+    'adcs_array_temp' / AffineAdapter(1/0.00762, 273.150/0.00762,
+                                      BitsInteger(16))[6],
     'adcs_css_raw' / BitsInteger(16)[6],
     'fss_active' / BitsInteger(2)[6],
     'css_active_selected' / BitsInteger(2)[6],
@@ -74,8 +119,8 @@ BeaconPartB = BitStruct(
     'cmc_rx_crc_error_count' / BitsInteger(16),
     'cmc_rx_overrun_error_count' / BitsInteger(8),
     'cmc_rx_protocol_error_count' / BitsInteger(16),
-    'cmc_smps_temperature' / Bytewise(Int8ub),
-    'cmc_pa_temperature' / Bytewise(Int8ub),
+    'cmc_smps_temperature' / Bytewise(SignMagnitude8),
+    'cmc_pa_temperature' / Bytewise(SignMagnitude8),
     'ax25_mux_channel_enable' / Flag[3],
     'digipeater_enable' / Flag,
     'pacsat_broadcast_enable' / Flag,
