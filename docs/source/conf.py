8e93fe82b9ee3042c29c26e4e9a01990ab8d803f
Daniel Estévez <daniel@destevez.net>
2021-09-11 18:34:37 +0200

    Version bump

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'Daniel Estévez'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '3.10.1'
+release = '3.11.0-git'
 
 
 # -- General configuration ---------------------------------------------------
