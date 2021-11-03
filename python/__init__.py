781f9301cf5b23ef6847c32d8a51572a9a34c90f
Daniel Estévez <daniel@destevez.net>
2021-11-03 19:49:17 +0100

    Version bump

--- python/__init__.py
+++ python/__init__.py
@@ -19,7 +19,7 @@ stack, the AO-40 protocol used in the FUNcube satellites, and several
 ad-hoc protocols used in other satellites.
 """
 
-__version__ = 'v3.11.0'
+__version__ = 'v3.12.0-git'
 __author__ = 'Daniel Estevez'
 __copyright__ = 'Copyright 2016-2021 Daniel Estevez'
 __email__ = 'daniel@destevez.net'
