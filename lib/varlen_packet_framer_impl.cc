fda6d4a5308bdeabb0d8f3c7e3dbde5444fafbee
Bjoern Kerler <info@revskills.de>
2022-03-30 15:55:36 +0200

    Update GRCs to GNU Radio 3.10
    This updates the GRC files in examples/ to GNU Radio 3.10. Additionally, it fixes a few GRC YAML file in grc/. It also adds a missing include for boost::format() in the varlen_packet_framer and varlen_packet_tagger blocks.

--- lib/varlen_packet_framer_impl.cc
+++ lib/varlen_packet_framer_impl.cc
@@ -14,10 +14,10 @@
 
 #include "varlen_packet_framer_impl.h"
 #include <gnuradio/io_signature.h>
+#include <boost/format.hpp>
 #include <cstdio>
 #include <ctime>
 #include <iostream>
-
 extern "C" {
 #include "golay24.h"
 #include "libfec/fec.h"
