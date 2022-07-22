8089f53893461f4063ba465c8bd75942fc287e8c
Daniel Estévez <daniel@destevez.net>
2022-07-22 22:07:07 +0200

    Up to v4.7.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'Daniel Estévez'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '4.7.0-git'
+release = '4.7.0'
 
 
 # -- General configuration ---------------------------------------------------
