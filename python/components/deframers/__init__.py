9f2e03ef0e3ac541f34425179d89842756606399
Daniel Estévez <daniel@destevez.net>
2022-07-31 11:44:40 +0200

    Add support for HSU-SAT1
    This adds a custom deframer for HSU-SAT1 and a SatYAML file.

--- python/components/deframers/__init__.py
+++ python/components/deframers/__init__.py
@@ -35,6 +35,7 @@ from .endurosat_deframer import endurosat_deframer
 from .eseo_deframer import eseo_deframer
 from .fossasat_deframer import fossasat_deframer
 from .grizu263a_deframer import grizu263a_deframer
+from .hsu_sat1_deframer import hsu_sat1_deframer
 from .ideassat_deframer import ideassat_deframer
 from .k2sat_deframer import k2sat_deframer
 from .lilacsat_1_deframer import lilacsat_1_deframer
