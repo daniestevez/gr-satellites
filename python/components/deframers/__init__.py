374d000184501954cfde65e4f05eeebd67167e70
Daniel Estévez <daniel@destevez.net>
2022-08-10 21:49:52 +0200

    Add support for GEOSCAN-EDELVEIS

--- python/components/deframers/__init__.py
+++ python/components/deframers/__init__.py
@@ -34,6 +34,7 @@ from .diy1_deframer import diy1_deframer
 from .endurosat_deframer import endurosat_deframer
 from .eseo_deframer import eseo_deframer
 from .fossasat_deframer import fossasat_deframer
+from .geoscan_deframer import geoscan_deframer
 from .grizu263a_deframer import grizu263a_deframer
 from .hsu_sat1_deframer import hsu_sat1_deframer
 from .ideassat_deframer import ideassat_deframer
