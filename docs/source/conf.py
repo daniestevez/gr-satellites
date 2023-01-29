d69a330288b18dabb84f9240c5cb659e1ccaa4ac
Daniel Estévez <daniel@destevez.net>
2023-01-29 09:40:16 +0100

    Up to v4.9.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,13 +20,13 @@
 # -- Project information -----------------------------------------------------
 
 project = 'gr-satellites'
-copyright = '2016-2022, Daniel Estévez'
+copyright = '2016-2023, Daniel Estévez'
 author = 'Daniel Estévez'
 
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '4.9.0-git'
+release = '4.9.0'
 
 
 # -- General configuration ---------------------------------------------------
