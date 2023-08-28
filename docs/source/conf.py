4e4f7e4a6b8d585a448e58a1fdf81eea499869c2
Daniel Estévez <daniel@destevez.net>
2023-08-28 22:44:09 +0200

    Fix version and release v3.18.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'Daniel Estévez'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '3.19.0-git'
+release = '3.18.1'
 
 
 # -- General configuration ---------------------------------------------------
