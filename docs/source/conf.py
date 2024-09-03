2abc7e70d2706ba746c3dddecab7126d2c267e0a
Daniel Estévez <daniel@destevez.net>
2024-09-03 19:36:03 +0200

    Up to v3.20.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'Daniel Estévez'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '3.20.0-git'
+release = '3.20.0'
 
 
 # -- General configuration ---------------------------------------------------
