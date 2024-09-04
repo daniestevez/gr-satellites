719e36f03283c66ebc5d184c5ba24d3fbd8adba5
Daniel Estévez <daniel@destevez.net>
2024-09-04 20:49:37 +0200

    Up to v5.7.0-git
    Also increment copyright year

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,13 +20,13 @@
 # -- Project information -----------------------------------------------------
 
 project = 'gr-satellites'
-copyright = '2016-2023, Daniel Estévez'
+copyright = '2016-2024, Daniel Estévez'
 author = 'Daniel Estévez'
 
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '5.6.0-git'
+release = '5.7.0-git'
 
 
 # -- General configuration ---------------------------------------------------
