23a743d11407411bce29c24548fefa9ab26826ea
Daniel Estévez <daniel@destevez.net>
2024-12-18 10:45:41 +0100

    Add vector stream IO Reed-Solomon encoder
    This modifies the encode_rs C++ block to add support for vector
    stream IO as an alternative to PDU IO. Stream IO can propagate
    backpressure to the source, so it is necessary to use stream IO
    instead of PDU IO when backpressure is needed.
    
    (cherry picked from commit 8838359882814fbbed9c5283e53771366dd4e95e)

--- lib/encode_rs_impl.h
+++ lib/encode_rs_impl.h
@@ -1,6 +1,6 @@
 /* -*- c++ -*- */
 /*
- * Copyright 2018,2020 Daniel Estevez <daniel@destevez.net>
+ * Copyright 2018,2020, 2024 Daniel Estevez <daniel@destevez.net>
  *
  * This file is part of gr-satellites
  *
@@ -28,28 +28,36 @@ private:
     std::vector<uint8_t> d_output_frame;
     int d_nroots;
     void* d_rs_p = NULL;
+    const int d_frame_size; // used only with vector stream IO
 
     std::function<void(uint8_t*)> d_encode_rs;
 
     constexpr static int d_ccsds_nn = 255;
     constexpr static int d_ccsds_nroots = 32;
 
+    void setup_ccsds(bool dual_basis);
+    void setup_generic(int symsize, int gfpoly, int fcr, int prim, int nroots);
     void check_interleave();
+    void check_frame_size();
     void set_message_ports();
 
 public:
     encode_rs_impl(bool dual_basis, int interleave = 1);
+    encode_rs_impl(int frame_size, bool dual_basis, int interleave = 1);
     encode_rs_impl(
         int symsize, int gfpoly, int fcr, int prim, int nroots, int interleave = 1);
-    ~encode_rs_impl();
+    encode_rs_impl(int frame_size,
+                   int symsize,
+                   int gfpoly,
+                   int fcr,
+                   int prim,
+                   int nroots,
+                   int interleave);
+    ~encode_rs_impl() override;
 
-    // Where all the action really happens
-    void forecast(int noutput_items, gr_vector_int& ninput_items_required);
-
-    int general_work(int noutput_items,
-                     gr_vector_int& ninput_items,
-                     gr_vector_const_void_star& input_items,
-                     gr_vector_void_star& output_items);
+    int work(int noutput_items,
+             gr_vector_const_void_star& input_items,
+             gr_vector_void_star& output_items) override;
 
     void msg_handler(pmt::pmt_t pmt_msg);
 };
