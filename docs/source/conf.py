44ab5f176ca15fb8085db9109ac8316cabb89645
Daniel Estévez <daniel@destevez.net>
2025-07-31 17:09:59 -0700

    Up to v5.9.0-git

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'Daniel Estévez'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '5.8.0-git'
+release = '5.9.0-git'
 
 
 # -- General configuration ---------------------------------------------------
