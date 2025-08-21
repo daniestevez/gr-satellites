7010c23a70ef5e69e3678e57fcb1bf29a4744309
Daniel Estévez <daniel@destevez.net>
2025-08-21 10:37:50 +0200

    Add packet length tag support to fixedlen_to_pdu
    This adds variable packet length support to fixedlen_to_pdu (so now
    the name of the block is almost a misnomer) by using an additional
    and optional packet length tag. The length specified in fixedlen_to_pdu
    is used both a maximum packet length (to size the internal history,
    for instance), and also as default packet length if the packet length
    tag is not present at the beginning of a packet or if this option is
    not used.
    
    The behaviour of the block remains the same if the packet length tag
    is not specified (if it is the empty string), so this change is not
    API breaking.
    
    Also, the fixedlen_to_pdu implementation has been slightly refactored,
    for instance by replacing the std::list used to hold the packet starts
    by a std::vector. In order to efficiently remove elements from this
    std::vector as they are consumed, the not consumed items are pushed back
    to another empty vector and the two vectors are swapped at the end of
    the loop.

--- lib/fixedlen_to_pdu_impl.cc
+++ lib/fixedlen_to_pdu_impl.cc
@@ -1,6 +1,6 @@
 /* -*- c++ -*- */
 /*
- * Copyright 2022 Daniel Estevez <daniel@destevez.net>
+ * Copyright 2022,2025 Daniel Estevez <daniel@destevez.net>
  *
  * This file is part of gr-satellites
  *
@@ -19,30 +19,31 @@ namespace satellites {
 fixedlen_to_pdu::sptr fixedlen_to_pdu::make(types::vector_type type,
                                             const std::string& syncword_tag,
                                             size_t packet_len,
-                                            bool pack)
+                                            bool pack,
+                                            const std::string& packet_length_tag_key)
 {
     return gnuradio::make_block_sptr<fixedlen_to_pdu_impl>(
-        type, syncword_tag, packet_len, pack);
+        type, syncword_tag, packet_len, pack, packet_length_tag_key);
 }
 
 fixedlen_to_pdu_impl::fixedlen_to_pdu_impl(types::vector_type type,
                                            const std::string& syncword_tag,
                                            size_t packet_len,
-                                           bool pack)
+                                           bool pack,
+                                           const std::string& packet_length_tag_key)
     : gr::sync_block("fixedlen_to_pdu",
                      gr::io_signature::make(1, 1, pdu::itemsize(type)),
                      gr::io_signature::make(0, 0, 0)),
       d_type(type),
+      d_itemsize(pdu::itemsize(d_type)),
       d_pack(pack),
       d_packetlen(packet_len),
-      d_packet_nbytes(packet_len * pdu::itemsize(type)),
-      d_pdu_items(pack ? packet_len / 8 : packet_len),
       d_syncword_tag(pmt::mp(syncword_tag)),
-      d_history((packet_len - 1) * pdu::itemsize(type)),
-      d_write_ptr_item(0),
-      d_write_ptr_byte(0),
-      d_packet(packet_len * pdu::itemsize(type)),
-      d_tag_offsets(0)
+      d_packet_length_tag(packet_length_tag_key.empty()
+                              ? std::nullopt
+                              : std::optional(pmt::mp(packet_length_tag_key))),
+      d_history((packet_len - 1) * d_itemsize),
+      d_packet(packet_len * d_itemsize)
 {
     if (pack && type != types::byte_t) {
         throw std::runtime_error("pack can only be used with byte_t");
@@ -61,59 +62,99 @@ int fixedlen_to_pdu_impl::work(int noutput_items,
 {
     auto in = static_cast<const uint8_t*>(input_items[0]);
 
-    get_tags_in_window(d_tags_in_window, 0, 0, noutput_items, d_syncword_tag);
-    for (auto tag : d_tags_in_window) {
-        d_tag_offsets.push_back(tag.offset);
+    std::vector<tag_t> tags;
+    get_tags_in_window(tags, 0, 0, noutput_items, d_syncword_tag);
+    for (const auto& tag : tags) {
+        packet_info info;
+        info.offset = tag.offset;
+        info.length = d_packetlen;
+        if (d_packet_length_tag.has_value()) {
+            std::vector<tag_t> tags;
+            get_tags_in_range(tags, 0, tag.offset, tag.offset + 1, *d_packet_length_tag);
+            if (!tags.empty()) {
+                const auto& value = tags[0].value;
+                if (pmt::is_uint64(value)) {
+                    info.length = pmt::to_uint64(value);
+                } else {
+                    info.length = pmt::to_long(value);
+                }
+                if (info.length > static_cast<ssize_t>(d_packetlen)) {
+                    d_logger->warn("length tag value {} larger than max length {}; "
+                                   "clamping to max length",
+                                   info.length,
+                                   d_packetlen);
+                    info.length = d_packetlen;
+                }
+            }
+        }
+        d_packet_infos.push_back(info);
     }
 
-    std::list<uint64_t> tag_offsets_copy = d_tag_offsets;
-    for (auto tag_offset : tag_offsets_copy) {
-        if (tag_offset + d_packetlen <= nitems_read(0) + noutput_items) {
+    d_new_packet_infos.clear();
+    for (const auto& info : d_packet_infos) {
+        if (info.offset + info.length <= nitems_read(0) + noutput_items) {
             // Packet ends in current input_items buffer
-            if (tag_offset >= nitems_read(0)) {
+            if (info.offset >= nitems_read(0)) {
                 // Packet doesn't use history
                 std::memcpy(d_packet.data(),
-                            &in[(tag_offset - nitems_read(0)) * pdu::itemsize(d_type)],
-                            d_packet_nbytes);
-            } else if (d_write_ptr_item + tag_offset >= nitems_read(0)) {
+                            &in[(info.offset - nitems_read(0)) * d_itemsize],
+                            info.length * d_itemsize);
+            } else if (d_write_ptr_item + info.offset >= nitems_read(0)) {
                 // Packet starts before the write pointer
-                size_t start = (d_write_ptr_item + tag_offset - nitems_read(0)) *
-                               pdu::itemsize(d_type);
-                size_t len = d_write_ptr_byte - start;
+                const size_t start =
+                    (d_write_ptr_item + info.offset - nitems_read(0)) * d_itemsize;
+                const size_t len =
+                    std::min(d_write_ptr_byte - start, info.length * d_itemsize);
                 std::memcpy(d_packet.data(), &d_history[start], len);
-                std::memcpy(&d_packet[len], &in[0], d_packet_nbytes - len);
+                const size_t remain = info.length * d_itemsize - len;
+                if (remain > 0) {
+                    std::memcpy(&d_packet[len], &in[0], remain);
+                }
             } else {
                 // Packet starts after the write pointer
-                size_t start =
-                    (d_write_ptr_item + tag_offset + d_packetlen - 1 - nitems_read(0)) *
-                    pdu::itemsize(d_type);
-                size_t len = d_history.size() - start;
+                const size_t start =
+                    (d_write_ptr_item + info.offset + info.length - 1 - nitems_read(0)) *
+                    d_itemsize;
+                const size_t len =
+                    std::min(d_history.size() - start, info.length * d_itemsize);
                 std::memcpy(d_packet.data(), &d_history[start], len);
-                std::memcpy(&d_packet[len], d_history.data(), d_write_ptr_byte);
-                std::memcpy(&d_packet[len + d_write_ptr_byte],
-                            &in[0],
-                            d_packet_nbytes - len - d_write_ptr_byte);
+                size_t remain = info.length * d_itemsize - len;
+                if (remain > 0) {
+                    const size_t len2 = std::min(d_write_ptr_byte, remain);
+                    std::memcpy(&d_packet[len], d_history.data(), len2);
+                    remain -= len2;
+                    if (remain) {
+                        std::memcpy(&d_packet[len + len2], &in[0], remain);
+                    }
+                }
             }
 
-            d_tag_offsets.erase(
-                std::find(d_tag_offsets.begin(), d_tag_offsets.end(), tag_offset));
+            const size_t pdu_items = d_pack ? info.length / 8 : info.length;
             if (d_pack) {
-                pack_packet();
+                if (info.length % 8 != 0) {
+                    d_logger->warn("packet size {} is not a multiple of 8 bits, "
+                                   "discarding remaining bits while packing");
+                }
+                pack_packet(pdu_items);
             }
             message_port_pub(
                 msgport_names::pdus(),
                 pmt::cons(pmt::PMT_NIL,
-                          pdu::make_pdu_vector(d_type, d_packet.data(), d_pdu_items)));
+                          pdu::make_pdu_vector(d_type, d_packet.data(), pdu_items)));
+        } else {
+            // End of packet not yet available. Save for next run
+            d_new_packet_infos.push_back(info);
         }
     }
+    d_packet_infos.swap(d_new_packet_infos);
 
     update_history(in, noutput_items);
     return noutput_items;
 }
 
-void fixedlen_to_pdu_impl::pack_packet()
+void fixedlen_to_pdu_impl::pack_packet(size_t size_bytes)
 {
-    for (size_t j = 0; j < d_pdu_items; ++j) {
+    for (size_t j = 0; j < size_bytes; ++j) {
         uint8_t b = 0;
         for (int k = 0; k < 8; ++k) {
             b <<= 1;
@@ -130,12 +171,12 @@ void fixedlen_to_pdu_impl::update_history(const uint8_t* in, int noutput_items)
     size_t len_items;
     const size_t history_items = d_packetlen - 1;
     if (static_cast<size_t>(noutput_items) > history_items) {
-        start = (noutput_items - history_items) * pdu::itemsize(d_type);
+        start = (noutput_items - history_items) * d_itemsize;
         len = d_history.size();
         len_items = history_items;
     } else {
         start = 0;
-        len = noutput_items * pdu::itemsize(d_type);
+        len = noutput_items * d_itemsize;
         len_items = noutput_items;
     }
     size_t len2;
