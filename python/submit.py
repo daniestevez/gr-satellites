4b11bdfbf6f4a71dd23ed7c6ec7b258a7f79ac62
Fabian P. Schmidt <kerel@mailbox.org>
2025-10-03 17:07:50 +0200

    pdu_to_kiss & submit: Refactor timezone handling
    Afer multiple iterations, patch as suggested in
    https://github.com/daniestevez/gr-satellites/pull/769#issuecomment-3375550009
    
    Fixes #752 and #769.

--- python/submit.py
+++ python/submit.py
@@ -9,6 +9,7 @@
 #
 
 import datetime
+import sys
 import urllib.error
 import urllib.parse
 import urllib.request
@@ -18,20 +19,53 @@ import pmt
 import numpy
 
 
-def parse_time(time):
-    try:
-        time = datetime.datetime.fromisoformat(time)
-
-        if time.tzinfo:
-            # Ensure time is converted to UTC, then drop tzinfo
-            time = time.astimezone(datetime.timezone.utc)
-            time = time.replace(tzinfo=None)
-
-        return time
-    except AttributeError:
-        # Workaround for Python version <3.7, which doesn't have
-        # fromisoformat()
-        return datetime.datetime.strptime(time, '%Y-%m-%dT%H:%M:%S')
+def parse_timestamp(s):
+    t = None
+    t_tz_frac = None
+    if sys.version_info < (3, 11):
+        # In Python 3.10, fromisoformat() does not accept a trailing Z, and it
+        # can only parse millisecond resolution. If there is fractional part,
+        # we remove any trailing Z, and parse the fractional seconds
+        # separately. Python 3.10 does not support fractional seconds in the
+        # timezone, so this is also handled separately.
+        s = s.rstrip('Z')
+        if '+' in s:
+            # s has a timezone
+            s, s_tz = s.split('+')
+            if '.' in s_tz:
+                # The timezone has a fractional seconds part. The fractional
+                # part is incorporated back near the end of this function.
+                s_tz, s_tz_frac = s_tz.split('.')
+                t_tz_frac = float(f'0.{s_tz_frac}')
+        else:
+            s_tz = None
+        if '.' in s:
+            # s has a fractional seconds part
+            s_int, s_frac = s.split('.')
+            if s_tz is not None:
+                # Put back the timezone in s_int
+                s_int = f'{s_int}+{s_tz}'
+            t_int = datetime.datetime.fromisoformat(s_int)
+            t_frac = float(f'0.{s_frac}')
+            # Truncate t_frac to integer microseconds to match behaviour of
+            # fromisoformat() in newer Python version
+            t = t_int + datetime.timedelta(
+                microseconds=int(t_frac * 1000000))
+        elif s_tz is not None:
+            # Put back the timezone in s
+            s = f'{s}+{s_tz}'
+    if t is None:
+        t = datetime.datetime.fromisoformat(s)
+    if t_tz_frac is not None:
+        t -= datetime.timedelta(microseconds=int(t_tz_frac * 1000000))
+    if t.tzinfo is None:
+        # The default timezone for this function is UTC
+        t = t.replace(tzinfo=datetime.timezone.utc)
+    else:
+        # Make sure that we are returning a datetime object for the UTC
+        # timezone, to avoid surprises by mixing timezones in downstream code
+        t = t.astimezone(datetime.timezone.utc)
+    return t
 
 
 class submit(gr.basic_block):
@@ -56,9 +90,9 @@ class submit(gr.basic_block):
             'version': '1.6.6',
             }
         self.initialTimestamp = (
-            parse_time(initialTimestamp)
+            parse_timestamp(initialTimestamp)
             if initialTimestamp != '' else None)
-        self.startTimestamp = datetime.datetime.utcnow()
+        self.startTimestamp = datetime.datetime.now(tz=datetime.timezone.utc)
 
         self.message_port_register_in(pmt.intern('in'))
         self.set_msg_handler(pmt.intern('in'), self.handle_msg)
@@ -78,11 +112,12 @@ class submit(gr.basic_block):
 
         self.request['frame'] = bytes(pmt.u8vector_elements(msg)).hex().upper()
 
-        now = datetime.datetime.utcnow()
-        timestamp = (
-            now - self.startTimestamp + self.initialTimestamp
-            if self.initialTimestamp else now)
-        self.request['timestamp'] = timestamp.isoformat()[:-3] + 'Z'
+        t_now = datetime.datetime.now(tz=datetime.timezone.utc)
+        t_prop = (
+            t_now - self.startTimestamp + self.initialTimestamp
+            if self.initialTimestamp else t_now)
+        t_prop_fmt = t_prop.replace(tzinfo=None).isoformat()[:-3] + 'Z'
+        self.request['timestamp'] = t_prop_fmt
 
         params = urllib.parse.urlencode(self.request)
         try:
