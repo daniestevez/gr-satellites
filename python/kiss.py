96dcb2dd9285917ddeceb20fe2cf6b85374bd8f4
Fabian P. Schmidt <kerel@mailbox.org>
2025-10-06 15:14:05 +0200

    Move read_kiss_file to python/kiss.py

--- python/kiss.py
+++ python/kiss.py
@@ -33,3 +33,27 @@ def kiss_escape(a):
         else:
             buff.append(np.uint8(x))
     return buff
+
+
+def read_kiss_file(path, framesize=None):
+    frames = list()
+    frame = list()
+    transpose = False
+    with open(path, 'rb') as f:
+        for c in f.read():
+            if c == FEND:
+                if ((framesize is None or len(frame) == framesize + 1)
+                        and len(frame) > 1 and (frame[0] & 0x0f) == 0):
+                    frames.append(frame[1:])
+                frame = list()
+            elif transpose:
+                if c == TFEND:
+                    frame.append(FEND)
+                elif c == TFESC:
+                    frame.append(FESC)
+                transpose = False
+            elif c == FESC:
+                transpose = True
+            else:
+                frame.append(c)
+    return np.array(frames, dtype='uint8')
